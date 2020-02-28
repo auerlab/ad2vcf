85d1c781105f00eb8ee5fa58b5028f2747109002
Jason Bacon <bacon@MacBook.local>
2020-02-28 09:37:30 -0600

    run-test.sh: Compare results to reference output

--- Test/run-test.sh
+++ Test/run-test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -e
 
 time ../ad2vcf test.vcf < test.sam
+
+printf "Comparing your results to the reference...\n"
+if diff -u test-ad-correct.vcf test-ad.vcf; then
+    printf "No differences found.\n"
+else
+    printf "Differences found.\n"
+fi
