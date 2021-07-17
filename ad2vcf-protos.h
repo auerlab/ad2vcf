b8230374812c3dd6d395325e67103f3a69165d09
Jason Bacon <bacon@moray.acadix.biz>
2021-07-16 19:44:39 -0500

    Update for biolibc bl_ prefix

--- ad2vcf-protos.h
+++ ad2vcf-protos.h
@@ -2,7 +2,7 @@
 int main(int argc, const char *argv[]);
 void usage(const char *argv[]);
 int ad2vcf(const char *argv[], FILE *sam_stream);
-int skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream, bl_sam_buff_t *sam_buff, FILE *vcf_out_stream, ad2vcf_stats_t *stats);
+int bl_skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream, bl_sam_buff_t *sam_buff, FILE *vcf_out_stream, ad2vcf_stats_t *stats);
 int allelic_depth(bl_vcf_t *vcf_call, FILE *sam_stream, bl_sam_buff_t *sam_buff, FILE *vcf_out_stream, ad2vcf_stats_t *stats);
 void update_allele_count(bl_vcf_t *vcf_call, bl_sam_t *sam_alignment, FILE *vcf_out_stream, ad2vcf_stats_t *stats);
 int uchar_cmp(unsigned char *c1, unsigned char *c2);
--- ad2vcf.c
+++ ad2vcf.c
@@ -101,7 +101,7 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
 	exit(EX_USAGE);
     }
     
-    sam_buff_init(&sam_buff, mapq_min);
+    bl_sam_buff_init(&sam_buff, mapq_min);
     ad2vcf_stats_init(&stats);
     
     printf("\nProcessing \"%s\", MAPQ min = %u:\n\n", vcf_filename, mapq_min);
@@ -130,10 +130,10 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
 	exit(EX_CANTCREAT);
     }
 
-    vcf_call_init(&vcf_call, BL_VCF_INFO_MAX_CHARS, BL_VCF_FORMAT_MAX_CHARS,
+    bl_vcf_call_init(&vcf_call, BL_VCF_INFO_MAX_CHARS, BL_VCF_FORMAT_MAX_CHARS,
 		  BL_VCF_SAMPLE_MAX_CHARS);
     
-    while ( vcf_read_ss_call(vcf_in_stream, &vcf_call, BL_VCF_FIELD_ALL) == BL_READ_OK )
+    while ( bl_vcf_read_ss_call(vcf_in_stream, &vcf_call, BL_VCF_FIELD_ALL) == BL_READ_OK )
     {
 	++stats.total_vcf_calls;
 	
@@ -148,7 +148,7 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
 	if ( strcmp(BL_VCF_CHROMOSOME(&vcf_call), previous_vcf_chromosome) == 0 )
 	{
 	    if ( BL_VCF_POS(&vcf_call) < previous_vcf_pos )
-		 vcf_out_of_order(&vcf_call, previous_vcf_chromosome,
+		 bl_vcf_out_of_order(&vcf_call, previous_vcf_chromosome,
 				  previous_vcf_pos);
 	    else
 		previous_vcf_pos = BL_VCF_POS(&vcf_call);
@@ -156,7 +156,7 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
 	else if ( bl_chromosome_name_cmp(BL_VCF_CHROMOSOME(&vcf_call),
 				      previous_vcf_chromosome) < 0 )
 	{
-	    vcf_out_of_order(&vcf_call, previous_vcf_chromosome,
+	    bl_vcf_out_of_order(&vcf_call, previous_vcf_chromosome,
 			     previous_vcf_pos);
 	}
 	else
@@ -170,7 +170,7 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
 	}
 	
 	/* Skip SAM alignments that don't include this position */
-	more_alignments = skip_upstream_alignments(&vcf_call, sam_stream,
+	more_alignments = bl_skip_upstream_alignments(&vcf_call, sam_stream,
 					       &sam_buff, vcf_out_stream,
 					       &stats) == BL_READ_OK;
 	
@@ -223,7 +223,7 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
     {
 	BL_SAM_BUF_INC_TOTAL_ALIGNMENTS(&sam_buff);
 	BL_SAM_BUF_INC_TRAILING_ALIGNMENTS(&sam_buff);
-	if ( !sam_buff_alignment_ok(&sam_buff, &sam_alignment) )
+	if ( !bl_sam_buff_alignment_ok(&sam_buff, &sam_alignment) )
 	    BL_SAM_BUF_INC_DISCARDED_TRAILING(&sam_buff);
     }
 #endif
@@ -294,7 +294,7 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
  *  2020-05-26  Jason Bacon Begin
  ***************************************************************************/
 
-int     skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream,
+int     bl_skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream,
 				 bl_sam_buff_t *sam_buff, FILE *vcf_out_stream,
 				 ad2vcf_stats_t *stats)
 
@@ -305,7 +305,7 @@ int     skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream,
     static bl_sam_t sam_alignment = BL_SAM_ALIGNMENT_INIT;
 
     if ( sam_alignment.seq == NULL )
-	sam_init_alignment(&sam_alignment, BL_SAM_SEQ_MAX_CHARS, REQUIRED_BL_SAM_FIELDS);
+	bl_sam_init_alignment(&sam_alignment, BL_SAM_SEQ_MAX_CHARS, REQUIRED_BL_SAM_FIELDS);
     
     /*
      *  Check and discard already buffered alignments upstream of the given
@@ -313,7 +313,7 @@ int     skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream,
      *  since the calls must be sorted in ascending order.
      */
     for (c = 0; (c < BL_SAM_BUFF_BUFFERED_COUNT(sam_buff)) &&
-	 vcf_call_downstream_of_alignment(vcf_call, BL_SAM_BUFF_ALIGNMENTS(sam_buff, c));
+	 bl_vcf_call_downstream_of_alignment(vcf_call, BL_SAM_BUFF_ALIGNMENTS(sam_buff, c));
 	 ++c)
     {
 #ifdef DEBUG
@@ -326,7 +326,7 @@ int     skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream,
     
     /* If anything to unbuffer, shift */
     if ( c > 0 )
-	sam_buff_shift(sam_buff, c);
+	bl_sam_buff_shift(sam_buff, c);
     
     /*
      *  Read alignments from the stream until we find one that's not upstream of
@@ -335,7 +335,7 @@ int     skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream,
      */
     if ( BL_SAM_BUFF_BUFFERED_COUNT(sam_buff) == 0 )
     {
-	while ( (ma = sam_read_alignment(sam_stream, &sam_alignment,
+	while ( (ma = bl_sam_read_alignment(sam_stream, &sam_alignment,
 					 REQUIRED_BL_SAM_FIELDS)) == BL_READ_OK )
 	{
 	    BL_SAM_BUFF_INC_TOTAL_ALIGNMENTS(sam_buff);
@@ -345,13 +345,13 @@ int     skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream,
 		    BL_SAM_SEQ_LEN(&sam_alignment), BL_SAM_QUAL_LEN(&sam_alignment),
 		    BL_SAM_MAPQ(&sam_alignment));
 	    */
-	    if ( sam_buff_alignment_ok(sam_buff, &sam_alignment) )
+	    if ( bl_sam_buff_alignment_ok(sam_buff, &sam_alignment) )
 	    {
 		/*
 		 *  We're done when we find an alignment overlapping or after
 		 *  the VCF call
 		 */
-		if ( ! vcf_call_downstream_of_alignment(vcf_call, &sam_alignment) )
+		if ( ! bl_vcf_call_downstream_of_alignment(vcf_call, &sam_alignment) )
 		    break;
 #ifdef DEBUG
 		else
@@ -367,7 +367,7 @@ int     skip_upstream_alignments(bl_vcf_t *vcf_call, FILE *sam_stream,
 		    BL_SAM_RNAME(&sam_alignment), BL_SAM_POS(&sam_alignment),
 		    BL_SAM_SEQ_LEN(&sam_alignment));
 #endif
-	sam_buff_add_alignment(sam_buff, &sam_alignment);
+	bl_sam_buff_add_alignment(sam_buff, &sam_alignment);
     }
 
     return ma;
@@ -396,11 +396,11 @@ int     allelic_depth(bl_vcf_t *vcf_call, FILE *sam_stream,
     static bl_sam_t sam_alignment = BL_SAM_ALIGNMENT_INIT;
 
     if ( sam_alignment.seq == NULL )
-	sam_init_alignment(&sam_alignment, BL_SAM_SEQ_MAX_CHARS, REQUIRED_BL_SAM_FIELDS);
+	bl_sam_init_alignment(&sam_alignment, BL_SAM_SEQ_MAX_CHARS, REQUIRED_BL_SAM_FIELDS);
 
     /* Check and discard already buffered alignments */
     for (c = 0; (c < BL_SAM_BUFF_BUFFERED_COUNT(sam_buff)) &&
-		(overlapping = vcf_call_in_alignment(vcf_call,
+		(overlapping = bl_vcf_call_in_alignment(vcf_call,
 		    BL_SAM_BUFF_ALIGNMENTS(sam_buff,c)));
 		++c)
     {
@@ -417,7 +417,7 @@ int     allelic_depth(bl_vcf_t *vcf_call, FILE *sam_stream,
     if ( (c == 0) || overlapping )
     {
 	/* Read and buffer more alignments from the stream */
-	while ( (ma = sam_read_alignment(sam_stream, &sam_alignment,
+	while ( (ma = bl_sam_read_alignment(sam_stream, &sam_alignment,
 					 REQUIRED_BL_SAM_FIELDS)) == BL_READ_OK )
 	{
 	    BL_SAM_BUFF_INC_TOTAL_ALIGNMENTS(sam_buff);
@@ -426,7 +426,7 @@ int     allelic_depth(bl_vcf_t *vcf_call, FILE *sam_stream,
 		    BL_SAM_RNAME(&sam_alignment), BL_SAM_POS(&sam_alignment),
 		    BL_SAM_SEQ_LEN(&sam_alignment), BL_SAM_MAPQ(&sam_alignment));
 	    */
-	    if ( sam_buff_alignment_ok(sam_buff, &sam_alignment) )
+	    if ( bl_sam_buff_alignment_ok(sam_buff, &sam_alignment) )
 	    {
 #ifdef DEBUG
 		fprintf(stderr, "depth(): Buffering new alignment #%zu %s,%zu,%zu\n",
@@ -434,9 +434,9 @@ int     allelic_depth(bl_vcf_t *vcf_call, FILE *sam_stream,
 			BL_SAM_RNAME(&sam_alignment),
 			BL_SAM_POS(&sam_alignment), BL_SAM_SEQ_LEN(&sam_alignment));
 #endif
-		sam_buff_add_alignment(sam_buff, &sam_alignment);
+		bl_sam_buff_add_alignment(sam_buff, &sam_alignment);
 		
-		if ( vcf_call_in_alignment(vcf_call, &sam_alignment) )
+		if ( bl_vcf_call_in_alignment(vcf_call, &sam_alignment) )
 		{
 #ifdef DEBUG
 		    fprintf(stderr, "depth(): Counting new alignment %s,%zu containing call %s,%zu\n",
