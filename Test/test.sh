cdb7f4fadd586986443424ed1569e9acceafc9e8
Jason Bacon <bacon@moray.acadix.biz>
2021-07-25 13:43:01 -0500

    Better test script interaction

--- Test/test.sh
+++ Test/test.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -e
 
+pause()
+{
+    local junk
+    
+    printf "Press return to continue..."
+    read junk
+}
+
 cd ..
 ./cave-man-install.sh
 cd Test
@@ -13,9 +21,9 @@ Comparing your results to the reference...
 
 EOM
 if diff -u test-ad-correct.vcf test-ad.vcf; then
-    printf "No differences found.\n"
+    printf "No differences found, test passed.\n"
 else
-    printf "Differences found.\n"
+    printf "Differences found, test failed.\n"
 fi
 mv test-ad.vcf test-ad-last.vcf
 
@@ -26,15 +34,21 @@ The following 4 tests should fail with complaints about input sorting.
 ======================================================================
 
 EOM
+pause
 
 set +e
 printf "=== test-bad-pos.vcf\n"
 ../ad2vcf test-bad-pos.vcf 10 < test.sam
+pause
+
 printf "=== test-bad-chr.vcf\n"
 ../ad2vcf test-bad-chr.vcf 10 < test.sam
+pause
 
 printf "=== test-bad-pos.sam\n"
 ../ad2vcf test.vcf 10 < test-bad-pos.sam
+pause
+
 printf "=== test-bad-chr.sam\n"
 ../ad2vcf test.vcf 10 < test-bad-chr.sam
 rm test-ad.vcf
